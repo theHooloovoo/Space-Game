97d910d48f9dfbf760627583f377bb5aa719442d
theHooloovoo <theHooloovoo@protonmail.com>
2018-09-26 10:03:21 -0400

    Added   - main.py:   Project entry point   - entity.py: Contains the Entity class   - img.png:   A test image

---
+++ entity.py
@@ -0,0 +1,11 @@
+
+import pygame
+from pygame.locals import *
+
+class Entity:
+    mass = 0.0
+    loc = [0.0, 0.0]
+    vel = [0.0, 0.0]
+
+    image = pygame.image.load("img.png")
+
---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import entity
+
+import sys
+# Use the pygame library
+import pygame
+from pygame.locals import *
+
+loc = [0.0, 0.0]
+vel = [1.0, 1.0]
+
+pygame.init()
+window = pygame.display.set_mode([1200, 800])
+
+image = pygame.image.load("img.png")
+image_rect = image.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    image_rect = image_rect.move(vel)
+
+    window.fill([0,0,0])
+    window.blit(image, image_rect)
+    pygame.display.flip()
+
