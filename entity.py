6070776084bd72bb4c7c2b3fee66026cea5704bf
theHooloovoo <theHooloovoo@protonmail.com>
2018-10-02 12:34:01 -0400

    added rotation and radius fields to entity, while removing the mass field.

--- entity.py
+++ entity.py
@@ -3,9 +3,10 @@ import pygame
 from pygame.locals import *
 
 class Entity:
-    mass = 0.0
     loc = [0.0, 0.0]
     vel = [0.0, 0.0]
+    rotation = 0
+    radius = 0
 
     image = pygame.image.load("img.png")
 
