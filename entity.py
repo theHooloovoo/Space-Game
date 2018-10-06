17278257054c0427fe569053c1bd5ec1bcad3c5c
theHooloovoo <theHooloovoo@protonmail.com>
2018-10-06 16:37:34 -0400

    Fleshed out the Entity class! Fully fledged 2d orbital mechanics now works! We'll probably just need to add a camera class get all proper panning when objects go out of bounds,  though.

--- entity.py
+++ entity.py
@@ -1,35 +1,121 @@
 
+import math
+from math import atan2, sin, cos
+
 import pygame
 from pygame.locals import *
 
+""" Base Class upon which the rest of the ingame objects are based. Contains
+    all of the basic methods needed to be displayed on screen, as well as all 
+    of the basic physics methods to drive movement. """
 class Entity:
-    def __init__(self, loc, vel, rotation, radius, img_path):
-        loc = loc   # list of two floating points (x, y)
-        vel = vel   # list of two floating points (x, y)
-        rotation = rotation
-        radius = radius
+    def __init__(self, loc, vel, radius, rotation, img_path):
+        self.loc = loc   # list of two floating points (x, y)
+        self.vel = vel   # list of two floating points (x, y)
+        self.force = [0.0, 0.0] # This is determined by the environment
+        self.radius = radius
+        self.rotation = rotation
+        self.image = pygame.image.load(img_path)
 
-        image = pygame.image.load(img_path)
+    """ Returns the euclidean distance to the given body, as a float. """
+    def distance_to(self, x, y):
+        dx = x - self.loc[0]
+        dy = y - self.loc[1]
+
+        return sqrt(dx*dx + dy*dy)
+
+    """ Same as the 'distance_to()' method, but doesn't square root the
+        answer. """
+    def distance_to_squared(self, body):
+        dx = body.loc[0] - self.loc[0]
+        dy = body.loc[1] - self.loc[1]
+
+        return dx*dx + dy*dy
+
+    """ Returns the difference in x and y coordinates from the given body, as
+        a list of floats. """
+    def delta_location(self, body):
+        dx = body.loc[0] - self.loc[0]
+        dy = body.loc[1] - self.loc[1]
+
+        return [dx, dy]
+
+    """ Returns True if self and body are touching. Collision detection upon
+        two circles. """
+    def touching(self, body):
+        d = self.distance_to(body)
+        if d <= self.radius + body.radius:
+            return True
+        else:
+            return False
+
+    """ Increments the Entity's location by the Entities experienced
+        velocity. """
+    def iterate_location(self, dt):
+        self.loc[0] += self.vel[0] * dt
+        self.loc[1] += self.vel[1] * dt
+
+    """ Increments the Entity's velocity by the Entities experienced force. """
+    def iterate_velocity(self, dt):
+        self.vel[0] += self.force[0] * dt
+        self.vel[1] += self.force[1] * dt
+
+    """ Iterates through a list of stars, calculating the gravitaional force
+        from each. Updates the Entity's force variable as the summation of
+        each calculated force. """
+    def iterate_force(self, star_list):
+        resultant_force = [0.0, 0.0] # Reset the entities force value
+        for body in star_list:
+            d = self.distance_to_squared(body)
+            if d != 0:  # Avoid division by zero!!
+                r = self.delta_location(body)
+                radians = atan2(r[1], r[0])   # Direction
+                magnitude = body.mass / d    # Magnitude
+                resultant_force[0] += magnitude * cos(radians) * 10
+                resultant_force[1] += magnitude * sin(radians) * 10
+        self.force = resultant_force
+
+    """ Given a window to draw on, this methods internally rotates, scales and
+        moves the Entity's image field, then paints it onto the window. 
+        Entity's image is scaled such that it's width is equal to the Entity's
+        radius.  """
+    def draw(self, window):
+        image_loc = self.image.get_rect().width
+        s = self.radius / self.image.get_rect().width
+        # Re-Transform the image for each frame
+        transform = pygame.transform.rotozoom(self.image, self.rotation, s)
+        image_loc = transform.get_rect()
+        # Offset the image so that it is centered at the objects location
+        offset = [
+                  self.loc[0] - image_loc.width/2.0,
+                  self.loc[1] - image_loc.height/2.0,
+                 ]
+        image_loc = image_loc.move(offset)
+        # Maybe add guards to check if image is outisde the window?
+        window.blit(transform, image_loc)
 
 class Enemy(Entity):
-    def __init__(self, speed, health):
-        speed = speed
-        health = health
+    def __init__(self, loc, vel, radius, rotation, booster_speed, health):
+        Entity.__init__(self, loc, vel, radius, rotation, img_path)
+        self.booster_speed = booster_speed
+        self.health = health
 
 class Player(Enemy):
-    def __init__(self, weapon):
-        weapon = weapon # Is a list of weapons
+    def __init__(self, weapons):
+        weapons = weapons # Is a list of weapons
 
 class Projectile(Entity):
     def __init__(self, speed, damage, duration):
-        speed = speed
-        damage = damage
-        duration = duration
+        # Entity.__init__(self, <-- Finish!!
+        self.speed = speed
+        self.damage = damage
+        self.duration = duration
         # Owner # How would this get implemented?
 
 # class Guided(Projectile):
 
 class Star(Entity):
-    def __init__(self, mass):
-        mass = 100
+    def __init__(self, loc, vel, radius, mass, img_path):
+        Entity.__init__(self, loc, vel, radius, 0, img_path)
+        self.mass = 100
 
--- main.py
+++ main.py
@@ -8,23 +8,48 @@ import sys
 import pygame
 from pygame.locals import *
 
-loc = [0.0, 0.0]
-vel = [1.0, 1.0]
-
 pygame.init()
 window = pygame.display.set_mode([1200, 800])
 
-image = pygame.image.load("img.png")
-image_rect = image.get_rect()
+# image = pygame.image.load("img.png")
+# image_rect = image.get_rect()
+
+ent_list = []
+star_list = []
+
+ent_list.append(Entity([400.0, 400.0], [0.00, 1.00], 10.0, 0.0, "img.png" ))
+ent_list.append(Entity([500.0, 400.0], [0.00, 3.50], 10.0, 45.0, "img.png" ))
+ent_list.append(Entity([110.0, 100.0], [0.50, -0.20], 10.0, 45.0, "img.png" ))
+ent_list.append(Entity([100.0, 110.0], [0.00, 0.50], 10.0, 45.0, "img.png" ))
+
+star_list.append(Star( [600.0, 400.0], [0.0, 0.0], 100.0, 100000.0, "img.png" ))
+star_list.append(Star( [1000.0, 700.0], [0.0, 0.0], 100.0, 1000.0, "img.png" ))
+
+delta_time = 5.0
 
 while 1:
+
+    # Quit when the window gets closed
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-    image_rect = image_rect.move(vel)
+    # Physics Loop ====================
+    for e in ent_list:
+        e.iterate_force(star_list)
 
+    for e in ent_list:
+        e.iterate_velocity(delta_time)
+
+    for e in ent_list:
+        e.iterate_location(delta_time)
+
+    # Paint ===========================
     window.fill([0,0,0])
-    window.blit(image, image_rect)
+    for s in star_list:
+        s.draw(window)
+    for e in ent_list:
+        e.draw(window)
+
     pygame.display.flip()
 
