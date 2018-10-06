a7d45f5fae3f5c45b34f37529be78fc17c8ae579
theHooloovoo <theHooloovoo@protonmail.com>
2018-10-06 18:11:46 -0400

    Oops, forgot the level.py file.

---
+++ level.py
@@ -0,0 +1,9 @@
+
+from entity import Entity, Enemy, Player, Projectile, Star
+
+class Level:
+    def __init__(self):
+        star_list = []
+        entity_list = []
+        projectile_list = []
+
